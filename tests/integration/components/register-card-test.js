9fe91ee9689fe2df6439ffa9a6dc19ebcbad4607
BT <bernardotolosajr@gmail.com>
2016-05-12 14:16:37 +0800

    login-register pages

---
+++ tests/integration/components/register-card-test.js
@@ -0,0 +1,17 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('register-card', 'Integration | Component | register card', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.render(hbs`
+    {{#register-card}}
+      template block text
+    {{/register-card}}
+  `);
+
+  assert.equal(this.$().text().trim().replace(/[\s\n]+/g, ''),
+                'RegisterwithPeepchatUsernamePasswordConfirmPassword');
+});
