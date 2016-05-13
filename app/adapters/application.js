9c545def5f624f35898107cfdc9818205b60b4f3
BT <bernardotolosajr@gmail.com>
2016-05-13 13:18:05 +0800

    room resources

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,7 +5,7 @@ import config from '../config/environment';
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
   host: config.DS.host,
   namespace: config.DS.namespace,
-  authorizer: 'authorizer:application',
+  authorizer: 'authorizer:oauth2',
   urlForCreateRecord(modelName/*, snapshot*/) {
     switch(modelName) {
        case 'user':
